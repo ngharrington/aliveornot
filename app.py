8a8998f9a6cce8c41696d258f7c03e4f65a2f38b
Neal Harrington <ngharrington@users.noreply.github.com>
2022-06-05 22:55:14 -0400

    Add an endpoint to retrieve data about a person via their internal id or via their name.
    Limitations: You need to get there name as it exist
    in the DB exactly right.
    
    Up to casing
    
    A name may not be unique, it may return multiple rows. We fail for now on that case.
    
    The name may not exist we will fail on that.
    
    ID may not exist we will fail on that.
    
    Some of those cases should be 404 errors (not found) rather than 500 (internal unspecfiied errors)

--- app.py
+++ app.py
@@ -3,10 +3,23 @@ from pydantic import BaseModel
 import sqlite3
 import os
 
-SQLITE_DB = os.environ.get("ALIVE_OR_NOT_SQLITE_DB")
+SQLITE_DB = os.environ.get("ALIVE_OR_NOT_SQLITE_DB", "db.sqlite")
+
+class DbConnectionError(Exception):
+    pass
+
+def get_db_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect(SQLITE_DB)
+    except sqlite3.OperationalError:
+        raise DbConnectionError(f"Could not open sqlite database at {SQLITE_DB}")
+    return conn
 
 
 class AliveSchema(BaseModel):
+    id: int
+    name: str
     is_alive: bool
 
 app = FastAPI()
@@ -15,9 +28,34 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/alive/{id}")
-async def alive(id: int):
-    """Given an ID for a person, return whether they are alive or not (tm)"""
+@app.get("/people/{id}")
+async def get_person(id: int) -> AliveSchema:
+    """Given an ID for a person, respond with information about that person"""
+    db = get_db_connection()
+    cur = db.cursor()
+    cur.execute("SELECT id, primaryName, case when deathYear is null then 1 else 0 end as is_alive from people where id=?", (id,))
+    row = cur.fetchone()
+    response = AliveSchema(
+        id=row[0],
+        name=row[1],
+        is_alive=row[2]
+    )
+    return response
     
-    
+@app.get("/people")
+async def get_person_by_name(name: str):
+    """Given an full name for a person, respond with information about that person"""
+    db = get_db_connection()
+    cur = db.cursor()
+    cur.execute("SELECT id, primaryName, case when deathYear is null then 1 else 0 end as is_alive from people where lower(primaryName)=lower(?)", (name,))
+    rows = cur.fetchmany()
+    if len(rows) > 1 or len(rows) == 0:
+        raise Exception("Didn't return a unique result.")
+    row = rows[0]
+    response = AliveSchema(
+        id=row[0],
+        name=row[1],
+        is_alive=row[2]
+    )
+    return response
 
