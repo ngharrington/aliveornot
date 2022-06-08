0734cef748daf226d264d91ebfe58cf347565e2d
Neal Harrington <ngharrington@users.noreply.github.com>
2022-06-07 23:05:02 -0400

    Starting some terraform configs

---
+++ terraform/compute.tf
@@ -0,0 +1,22 @@
+data "digitalocean_ssh_key" "neal" {
+  name = "neal-xps"
+}
+
+resource "digitalocean_floating_ip" "aliveornot-floating-ip" {
+  region = "nyc3"
+}
+
+resource "digitalocean_droplet" "aliveornot-01" {
+  image  = "ubuntu-20-04-x64"
+  name   = "aliveornot-01"
+  region = "nyc3"
+  size   = "s-1vcpu-1gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.neal.id
+  ]
+}
+
+resource "digitalocean_floating_ip_assignment" "foobar" {
+  ip_address = digitalocean_floating_ip.aliveornot-floating-ip.ip_address
+  droplet_id = digitalocean_droplet.aliveornot-01.id
+}
---
+++ terraform/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+  backend "s3" {
+    endpoint = "nyc3.digitaloceanspaces.com"
+    region = "us-west-1" # this is ignored for a digital ocean space
+    key = "terraform.tfstate"
+    # skip_requesting_account_id = true
+    skip_credentials_validation = true
+    # skip_get_ec2_platforms = true
+    skip_metadata_api_check = true
+    bucket = "aliveornot"
+  }
+}
+
