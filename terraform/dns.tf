191aeaa968334a190c048c8f957c705fef503038
Neal Harrington <ngharrington@users.noreply.github.com>
2022-06-08 03:20:07 -0400

    DNS record

---
+++ terraform/dns.tf
@@ -0,0 +1,13 @@
+variable "domain" {}
+
+resource "digitalocean_domain" "default" {
+  name = var.domain
+}
+
+# Add an A record to the domain for www.example.com.
+resource "digitalocean_record" "www" {
+  domain = digitalocean_domain.default.id
+  type   = "A"
+  name   = "www"
+  value  = digitalocean_floating_ip.aliveornot-floating-ip.ip_address
+}
