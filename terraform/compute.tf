bad011048a567b9908468d045d41acf15f2f4690
Neal Harrington <ngharrington@users.noreply.github.com>
2022-06-08 02:48:02 -0400

    Enable floating IP so I can create DNS records using it

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -4,9 +4,9 @@
 #   name = "neal-xps"
 # }
 
-# resource "digitalocean_floating_ip" "aliveornot-floating-ip" {
-#   region = "nyc3"
-# }
+resource "digitalocean_floating_ip" "aliveornot-floating-ip" {
+  region = "nyc3"
+}
 
 # resource "digitalocean_droplet" "aliveornot-01" {
 #   image  = "ubuntu-20-04-x64"
