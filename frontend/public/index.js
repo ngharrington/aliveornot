affc38c9618120c9e9528a51f59c6f58ba8cf95b
Neal Harrington <ngharrington@users.noreply.github.com>
2022-06-17 20:47:30 +0000

    Working State

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -3,7 +3,7 @@ var result = document.getElementById('aon-result');
 a.addEventListener('submit',function(e) {
     e.preventDefault();
     var inputName = document.getElementById('aon-textfield').value;
-    fetch("http://localhost:8080/api/people?" + new URLSearchParams({"name": inputName}))
+    fetch("https://aliveornot.net/api/people?" + new URLSearchParams({"name": inputName}))
         .then(response => response.json())
         .then(data => {
             is_alive = data.is_alive ? 'ALIVE' : 'NOT'
