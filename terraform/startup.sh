6d097b752b51202c1a1a4a6a1c26b2aaf0ce6455
Neal Harrington <ngharrington@users.noreply.github.com>
2022-07-08 15:34:37 -0400

    Clean these up

--- terraform/startup.sh
+++ terraform/startup.sh
@@ -17,3 +17,22 @@ s3cmd get s3://aliveornot/aliveornot/db.sqlite /home/aliveornot/build/db.sqlite
 # make aliveornot user the owner
 sudo chown -R aliveornot:aliveornot /home/aliveornot/build
 
+# set up the systemd unit for the compose service
+cat >/etc/systemd/system/docker-compose@.service <<EOL
+[Unit]
+Description=%i service with docker compose
+PartOf=docker.service
+After=docker.service
+
+[Service]
+Type=oneshot
+User=aliveornot
+Group=aliveornot
+RemainAfterExit=true
+WorkingDirectory=/home/aliveornot/build
+ExecStart=/usr/bin/docker compose up -d --remove-orphans
+ExecStop=/usr/bin/docker compose down
+
+[Install]
+WantedBy=multi-user.target
+EOL
