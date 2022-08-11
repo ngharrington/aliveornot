751e1cd48953838263d2ce522934c05c5d1b322e
Neal Harrington <ngharrington@users.noreply.github.com>
2022-08-11 02:35:58 -0400

    Some frontend progress

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
@@ -19,11 +19,10 @@ def get_db_connection():
     return conn
 
 
-class AliveSchema(BaseModel):
+class Person(BaseModel):
     id: int
     name: str
-    is_alive: bool
-    unique_result: bool
+    is_alive: Optional[bool]
 
 app = FastAPI()
 
@@ -31,38 +30,43 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/api/people")
+async def search_by_name(search: str) -> List[Person]:
+    """Given an full name for a person, respond with information about that person"""
+    db = get_db_connection()
+    cur = db.cursor()
+    # since this is crude for now we just pick a random result. This must be improved.
+    query = """
+    SELECT nconst, primaryName
+    FROM alive
+        WHERE substr(lower(primaryName), 0, length(lower(?))+1)=lower(?)
+    ORDER BY totalVotes desc
+    """
+    cur.execute(query, (search, search))
+    rows = cur.fetchmany(5)
+    response = []
+    for row in rows:
+        response.append(
+            Person(
+                id=row[0],
+                name=row[1],
+                is_alive=None,)
+        )
+    return response
+
+
+
 @app.get("/api/people/{id}")
-async def get_person(id: int) -> AliveSchema:
+async def get_person(id: int) -> Person:
     """Given an ID for a person, respond with information about that person"""
     db = get_db_connection()
     cur = db.cursor()
     cur.execute("SELECT nconst, primaryName, alive as is_alive from alive where nconst=?", (id,))
     row = cur.fetchone()
-    response = AliveSchema(
+    response = Person(
         id=row[0],
         name=row[1],
         is_alive=row[2]
     )
     return response
     
-@app.get("/api/people")
-async def get_person_by_name(search: str) -> List[AliveSchema]:
-    """Given an full name for a person, respond with information about that person"""
-    db = get_db_connection()
-    cur = db.cursor()
-    # since this is crude for now we just pick a random result. This must be improved.
-    cur.execute("SELECT nconst, primaryName, alive as is_alive from alive where lower(primaryName)=lower(?) order by totalVotes desc", (search,))
-    rows = cur.fetchmany(2)
-    try:
-        row = rows[0]
-    except IndexError:
-        return []
-    unique_result = len(rows) == 1
-    response = [AliveSchema(
-        id=row[0],
-        name=row[1],
-        is_alive=row[2],
-        unique_result=unique_result,
-    )]
-    return response
-
